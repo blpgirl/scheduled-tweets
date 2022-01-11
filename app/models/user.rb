1eec16ba0602f34c28a74cd68c36bbc4d0113533
Leyla Bonilla <leyla.bonilla@bairesdev.com>
2022-01-11 14:08:11 -0500

    users sign up functionality

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :password, presence: true
+  validates :email, presence: true,
+      format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "must be a valid email address"}
+end
